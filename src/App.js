aa034e901cd42011866f3f0cf921ea6f7d087b33
Masis <masiskar@gmail.com>
2020-05-21 21:04:20 +0400

    HOC, lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 // import Person from './components/Person';
 // import greet, {idGen as idGenerator, Hello, getThis} from './tools';
 // import * as newObj from './tools';
@@ -10,22 +10,55 @@ import ToDo from './components/ToDo';
 // import Input from './components/Input';
 // newObj.getThis();
 // getThis();
+import Div from './components/HOC/Div';
 
 
 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log('App constructor');
+    this.state = {
+      counter: 0
+    };
+}
 
-function App() {
+componentDidMount(){
+  console.log('App mounted');
+}
+
+
+handleClick = ()=>{
+  this.setState({
+    counter: this.state.counter+1
+  });
+}
+
+  render(){
+console.log('App render starts');
 
   return (
     <div className={'App'}>
-{/* <img src={logo} alt=""/>
-<img src={require('./logo.svg')} alt=""/>
- */}
-    {/* <Input/> */}
+
+<button
+onClick = {this.handleClick}
+>
+Click me and see the magic!</button>
+
+<Div text='hello' className={classes['color-red']}>
+<span >span text</span>
+
+ <p>Lorem ipsum dolor sit amet.</p>
+ <a href="/">Click me</a>
+</Div>
+
 <ToDo/>
-{/* <Counter/> */}
+
     </div>
   );
+
+  }
+
 }
 
 export default App;
